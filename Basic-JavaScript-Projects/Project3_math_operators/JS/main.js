0dae544f2ce4c8fa62b35e6245574285756f5d70
CoderFrancis <mfcollegeacc@gmail.com>
2023-01-12 11:14:51 -0500

    20230112 @1115

---
+++ Basic-JavaScript-Projects/Project3_math_operators/JS/main.js
@@ -0,0 +1,12 @@
+function addition_Function()    {
+    var addition = 2 + 2;
+    document.getElementById("Math").innerHTML = "2 + 2 = " + addition;
+}
+
+var Math = myFunction(7, 3);
+document.getElementById("Math").innerHTML = x;
+
+function myFunction(1, 3) {
+  return a * b;
+}
+
